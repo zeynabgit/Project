4781e2ba89db8be21a47f07dc3ff55c9c0a3bf8b
SafaBabaei <zeyneb.babaei@gmail.com>
2023-03-05 12:16:58 -0500

    changes for firtsB class has been added to firts Branch

---
+++ src/main/java/org/example/FirstB.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FirstB {
+    public void firstBranch(){
+        System.out.println("First branch that I am working on it");
+        String name ="first Name";
+        System.out.println("name is : " + name);
+    }
+}
