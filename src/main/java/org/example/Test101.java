a9a54b3f1173b37bc8c03ecfd0f97bc3e4de8f12
SafaBabaei <zeyneb.babaei@gmail.com>
2023-03-22 13:04:46 -0400

    Class Test101 is added to branch Test101

---
+++ src/main/java/org/example/Test101.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Test101 {
+    public static void main(String[] args) {
+        System.out.println("Test101 should be added to te new branch which is called Test 101");
+    }
+}
